20bb907e1773e904dadcf234d857b06a14ec2c62
Dylan <liuxiangyu0613@gmail.com>
2021-02-25 14:51:38 -0800

    redesign frame

---
+++ src/edu/scu/GameWindow.java
@@ -0,0 +1,20 @@
+package edu.scu;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame{
+	
+	public GameWindow() {
+		this.initFrame();
+	}
+	
+	private void initFrame() {
+		this.setName("2048 Game");
+		this.setSize(500, 600);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		
+	}
+
+}
